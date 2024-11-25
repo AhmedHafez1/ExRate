2e27be20bf4b6cab177a6a0c6f6e76323348aff7
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-11-25 09:42:23 +0200

    cache

--- src/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ src/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296b2f9af7b6107d8da2138aa48f66a47661fccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2c1eef8dc331256414c9b2b1a0ceb4f69f70e42")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
